62e58d6783a2bef51b364d9a7f5ca6759ee81b7b
Letícia Santos <104040490+1Leticia24@users.noreply.github.com>
2023-02-02 23:06:03 -0300

    Add files via upload

---
+++ MiniCamp Dia 1.js
@@ -0,0 +1,15 @@
+alert("Óla Mundo!")
+
+const nomeUsuario = prompt('Me chmao Letícia, qual o seu nome? ');
+
+alert(`Bem Vindo ao meu primeiro programa ${nomeUsuario} &#128515;`);
+
+
+
+// alert("Óla mulheres maravilhosas!");
+
+//prompt("digite seu nome");
+
+//const nomeUsuario = prompt("Digite dua cantora favorita");
+
+//alert(`Bem Vinda ao meu Mundo ${nomeUsuario}`);
